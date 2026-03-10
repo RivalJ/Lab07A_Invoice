eae9146ce0e876d471cca394a1d3935c29853806
RivalJ <157822657+RivalJ@users.noreply.github.com>
2026-03-10 16:11:55 -0400

    Base object classes finished, moving to gui

--- src/Invoice.java
+++ src/Invoice.java
@@ -8,8 +8,14 @@ public class Invoice {
     public Invoice(Customer customer, List<LineItem> lineItems) {
         this.customer = customer;
         this.lineItems = lineItems;
+        total = 0;
     }
     public double GetTotal() {
+        if( total == 0 ) {//only calculate the total if it hasn't been calculated yet
+            for (LineItem item : lineItems) {
+                total += item.GetSubtotal();
+            }
+        }
         return total;
     }
     public Customer GetCustomer() {
--- src/InvoiceGUI.java
+++ src/InvoiceGUI.java
@@ -1,2 +1,11 @@
-public class InvoiceGUI {
+import javax.swing.*;
+import java.util.List;
+
+public class InvoiceGUI extends JFrame {
+    Invoice invoice;
+    List<LineItem> lineItems;
+
+    public InvoiceGUI() {
+        super("Invoice");
+    }
 }
